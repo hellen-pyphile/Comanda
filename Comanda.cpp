fe183b7fb34f7bcea0e240f800065eb42e59faa3
Hellen <hds74062@gmail.com>
2026-05-29 15:48:52 -0300

    feat: add faddorder function

--- Comanda.cpp
+++ Comanda.cpp
@@ -1,5 +1,6 @@
-//adicionar codigos de identificacao em cada prato
-//formatar o menu!!
+//NEXT STEP: ARRAY PRICES
+//ATTACH CODES ON INPUT
+//ATTACH/STORE ON TAB
 
 #include <stdio.h>
 #include <string>
@@ -8,11 +9,15 @@
 void fmenu();
 void fmenuop();
 void faddorder();
+//void ftotal();
 
 int main()
 {
 	printf("\tBon Apetit\n\n");
+	
 	fmenu();
+	printf("\n\nAdicionar pedido.\n");
+	faddorder();
 	fmenuop();
 }
 
@@ -25,10 +30,10 @@ void fmenu()
 	const char *dessert[] = {"Milkshake", "Petit Gateu", "Sorvete"};
 	
 	//menu codes
-	const char *cdapp[] = {"#011", "#021", "#031"};
-	const char *cdds[] = {"#012", "#022", "#032", "#042", "#052"};
-	const char *cddk[] = {"#013", "#023", "#033", "#043"};
-	const char *cddt[] = {"#014", "#024", "#034"};
+	const char *cdapp[] = {"#084", "#037", "#091"};
+	const char *cdds[]  = {"#015", "#072", "#048", "#066", "#029"};
+	const char *cddk[]  = {"#053", "#004", "#088", "#019"};
+	const char *cddt[]  = {"#077", "#042", "#061"};
 	
 	
 	//array size and print appetizer 
@@ -65,12 +70,11 @@ void fmenu()
 void fmenuop()
 {
 	int option;
-	printf("Digite uma opcao:\n\n");
-	printf("1. Adicionar novo pedido.\n");
+	printf("\n1. Adicionar novo pedido.\n");
 	printf("2. Editar pedido.\n");
 	printf("3. Cancelar pedido.\n");
-	printf("4. Encerrar pedido.\n");
-	
+	printf("4. Encerrar pedido.");
+	printf("\nDigite uma opcao: ");
 	scanf("%d", &option);
 	
 	if((option > 4) || (option <= 0))
@@ -78,25 +82,118 @@ void fmenuop()
 		printf("Digite uma opcao valida!");
 	}
 	
-	/*switch (op)
+	switch (option)
 	{
 		case 1:
 			faddorder();
 			break;
-		case 2:
-			feditorder();
-			break;
-		case 3:
-			fcancorder();
-			break;
-		case 4:
-			fendorder();
-			break;
-	}*/
+//		case 2:
+//			feditorder();
+//			break;
+//		case 3:
+//			fcancorder();
+//			break;
+//		case 4:
+//			fendorder();
+//			break;
+	}
 }
 
 void faddorder()
 {
+	char apporder[4];
+	char dsorder[4];
+	char dkorder[4];
+	char dtorder[4];
+	int appamt, dsamt, dkamt, dtamt;
+	int appnew, dsnew, dknew, dtnew;
 	
+	do
+	{
+		printf("\nDigite o codigo da entrada (0 para nenhum): ");
+		scanf("%s", &apporder);
+		
+		printf("Quantidade: ");
+		scanf("%d", &appamt);
+		//ADD QUANTITY ON PRICE
+		
+		printf("Deseja adicionar uma nova entrada? [1]Sim [2]Nao: ");
+		scanf("%d", &appnew);
+		
+		if(appnew > 2)
+		{
+			printf("Digite uma entrada valida!\n");
+		}
+		if(appnew == 2)
+			break;
+			
+	}while(appnew == 1);
+	
+	do
+	{
+		printf("\nDigite o codigo do prato (0 para nenhum): ");
+		scanf("%s", &dsorder);
+		
+		printf("Quantidade: ");
+		scanf("%d", &dsamt);
+		//ADD QUANTITY ON PRICE
+		
+		printf("Deseja adicionar uma novo prato? [1]Sim [2]Nao: ");
+		scanf("%d", &dsnew);
+		
+		if(dsnew > 2)
+		{
+			printf("Digite uma entrada valida");
+		}
+		if(dsnew == 2)
+			break;
+			
+	}while(dsnew == 1);
+	
+	do
+	{
+		printf("\nDigite o codigo da bebida (0 para nenhum): ");
+		scanf("%s", &dkorder);
+		
+		printf("Quantidade: ");
+		scanf("%d", &dkamt);
+		//ADD QUANTITY ON PRICE
+		
+		printf("Deseja adicionar uma nova entrada? [1]Sim [2]Nao: ");
+		scanf("%d", &dknew);
+		
+		if(dknew > 2)
+		{
+			printf("Digite uma entrada valida");
+		}
+		if(dknew == 2)
+			break;
+			
+	}while(dknew == 1);
+	
+	do
+	{
+		printf("\nDigite o codigo da sobremesa (0 para nenhum): ");
+		scanf("%s", &dtorder);
+		
+		printf("Quantidade: ");
+		scanf("%d", &dtamt);
+		//ADD QUANTITY ON PRICE
+		
+		printf("Deseja adicionar uma nova entrada? [1]Sim [2]Nao: ");
+		scanf("%d", &dtnew);
+		
+		if(dtnew < 2)
+		{
+			printf("Digite uma entrada valida");
+		}
+		if(dtnew == 2)
+			break;
+			
+	}while(dtnew == 1);
 }
 
+feditorder()
+{
+	
+}
